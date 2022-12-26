09639caa3d5225bb3f52887fe2ad676ac0ef46ec
bobstoun <119942184+bobstoun@users.noreply.github.com>
2022-12-26 16:22:24 +0300

    урааа

--- Program.cs
+++ Program.cs
@@ -1,11 +1,43 @@
 ﻿
-for (int i = 2; i <= 9;i++ )
+
+//int FiLLArray = (int[,]matr);
+//  for (int og = 0; og < matr.GetLength(0); og++)
+  
+//  for (int oe = 0; oe < matr.GetLength(1); oe++)
+
+//  {
+  // Console.Write($"{matr [og, oe]}");
+//  }
+
+  // Console.WriteLine();
+  //}   
+int[,] cip =  new int[,]
 {
- for(int j = 2; j <= 9;j++)
+{0,1,1,1,0,0,0,0,0,1,1,1,0,0},
+{0,1,1,1,2,0,0,0,0,1,1,1,0,0},
+{0,0,0,0,0,0,0,1,0,0,0,0,0,0},
+{1,1,0,0,0,0,1,1,1,0,0,0,1,1},
+{0,1,1,0,0,0,0,0,0,0,0,1,1,0},
+{0,0,1,1,1,1,1,1,1,1,1,1,0,0},
+};
+  void Prontcip (int[,]cip)
+
+  {
+   for (int i = 0; i < cip.GetLongLength(0);i++)
 
 {
-    Console.WriteLine($"{i}*{j} = {i*j}");
+        for (int j = 0; j < cip.GetLength(1);j++)
+  {
+    if (cip[i,j] == 0) Console.Write($" ");
+    
+      else Console.Write($"+");
+  } 
+      Console.WriteLine();   
 }
-Console.WriteLine();
+  }
+  
+Prontcip(cip);
 
-}
+ 
+
+
